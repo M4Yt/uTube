7119d661a25ded5f369a9fec4e66b5a5315fc314
PolyFloyd <floyd@polyfloyd.net>
2013-11-26 23:18:49 +0100

    Added persistent channel storage

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+Array.prototype.remove = function(what) {
+	while ((i = this.indexOf(what)) !== -1) {
+		this.splice(i, 1);
+	}
+    return this;
+};
+
 Storage.prototype.setObject = function(key, value) {
     this.setItem(key, JSON.stringify(value));
 }
@@ -6,28 +13,39 @@ Storage.prototype.getObject = function(key) {
     return JSON.parse(this.getItem(key));
 }
 
-var ut = utube || {}
+var utube = {
 
-ut.getChannels = function()
-{
-	return localstorage.getObject("channels") || [];
-}
+	channels: {
 
-ut.storeChannels = function(channels)
-{
-	return localstorage.setObject("channels", channels);
-}
+		getAll: function() {
+			return localStorage.getObject("channels") || [];
+		},
 
-ut.addChannel = function(channelName)
-{
-	var channels = utube.getChannels();
-	channels.push(channelName);
-	utube.storeChannels(channels);
-}
+		store: function(channels) {
+			return localStorage.setObject("channels", channels);
+		},
 
-ut.removeChannel = function(channelName)
-{
-	var channels = utube.getChannels();
-	delete channels["channelName"]
-	utube.storeChannels(channels);
-}
+		exists: function(channelName) {
+			return localStorage.getObject("channels").indexOf(channelName) !== -1;
+		},
+
+		add: function(channelName)
+		{
+			var isNew = !utube.channels.exists(channelName);
+			if (isNew) {
+				var channels = utube.channels.getAll();
+				channels.push(channelName);
+				utube.channels.store(channels);
+			}
+			return isNew;
+		},
+
+		remove: function(channelName) {
+			var channels = utube.channels.getAll();
+			channels.remove(channelName);
+			utube.channels.store(channels);
+		}
+
+	}
+
+};
