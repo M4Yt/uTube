c5f6ce71e8890ab64d35c54191447707edc16828
PolyFloyd <floyd@polyfloyd.net>
2014-09-08 23:18:37 +0200

    videoinfo.php will now use SSL to fetch pages

--- videoinfo.php
+++ videoinfo.php
@@ -15,7 +15,7 @@ header("Content-Type: text/plain");
 header("Access-Control-Allow-Origin: *");
 $id = isset($_GET['id']) ? $_GET['id'] : "";
 if (preg_match("/[a-zA-Z0-9_-]{11}/", $id) && (!isset($PASSWD) || $_GET['passwd'] === $PASSWD)) {
-  echo file_get_contents("http://youtube.com/watch?v=" . $_GET['id']);
+  echo file_get_contents("https://youtube.com/watch?v=" . $_GET['id']);
 } else {
   echo "Oh Long Johnson";
 }
